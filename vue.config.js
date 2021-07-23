280c162d5249d8680eaa7d5eb00fef48b8a1176d
Maenstreamy <menstra67@gmail.com>
2021-07-23 18:12:40 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   publicPath: "/comtek-test/",
   outputDir: "docs",
-=======
-  publicPath: "/",
->>>>>>> 6dcd1d69625b17b7c96edbd0c5a913a46aaadf67
   transpileDependencies: ["vuetify"],
 };
